b531b1aa2c79547a7b632d5621410aa6d72e8327
Fábio Gonçalves de Camargo <fabiogdecamargo@gmail.com>
2020-04-19 19:37:01 -0300

    Entidade OrderItem. Associação muitos para muitos com dados extras - parte 2

---
+++ src/main/java/com/br/winnersystems/curso/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.br.winnersystems.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.winnersystems.curso.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}

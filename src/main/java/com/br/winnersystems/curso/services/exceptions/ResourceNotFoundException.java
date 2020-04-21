87ccdebb0992cb8df4e71aa4f427393d500adefa
Fábio Gonçalves de Camargo <fabiogdecamargo@gmail.com>
2020-04-21 14:23:27 -0300

    Tratamento de exceção - delete

--- src/main/java/com/br/winnersystems/curso/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/br/winnersystems/curso/services/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package services.exceptions;
+package com.br.winnersystems.curso.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
